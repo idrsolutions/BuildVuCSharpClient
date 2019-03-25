1dc69d86ec6ae18c208f0046980724fb3fa76e18
Daniel Warren <danielwarren@idrsolutions.com>
2019-03-25 15:09:58 +0000

    Update Examples with latest implementation

--- ExampleUsage.cs
+++ ExampleUsage.cs
@@ -1,5 +1,5 @@
 using System;
-using BuildVu;
+using buildvu_csharp_client;
 
 class ExampleUsage
 {
@@ -7,12 +7,43 @@ class ExampleUsage
     {
         var buildvu = new BuildVu("http://localhost:8080/microservice-example");
 
-        // Convert() returns a URL (string) where you can view the converted output.
-        var outputURL = buildvu.Convert("path/to/input.pdf");
-        // or alternatively to send url for server to download file from
-        // var outputURL = buildvu.Convert("http://link.to/file.pdf", inputType: BuildVu.DOWNLOAD);
+        try
+        {
+            // Prepare a local file to be uploaded to the BuildVu MicroService
+            buildvu.PrepareFile("path/to/input.pdf");
 
-        // You can also specify a directory to download the converted output to:
-        // buildvu.Convert("path/to/input.pdf", "path/to/output/dir");
+            // Convert takes a Dictionary of the API parameters, that then get passed onto 
+            // the server. For example, callbackUrl will provide a URL that you want to have
+            // a request sent to when the conversion finishes.
+            // See https://github.com/idrsolutions/buildvu-microservice-example/blob/master/API.md
+            Dictionary<string, string> parameters = new Dictionary<string, string>
+            {
+                ["input"] = BuildVu.UPLOAD,
+                ["callbackUrl"] = "http://listener.url"
+            };
+
+            // Alternatively send a URL for the server to download file from
+            // Note: You do not require to PrepareFile() if you use this configuration
+            /*Dictionary<string, string> parameters = new Dictionary<string, string>
+            {
+                ["input"] = BuildVu.DOWNLOAD,
+                ["url"] = "http://link.to/filename"
+            };*/
+
+            // Convert() returns a Dictionary (<string, string>) which returns the values 
+            // in the servers response
+            Dictionary<string, string> conversionResults = buildvu.Convert(parameters);
+
+            String outputUrl = conversionResults.GetValueOrDefault("previewUrl", "No output URL provided");
+
+            // You can also specify a directory to download the converted output to:
+            //buildvu.DownloadResult(conversionResults, "path/to/output/dir");
+
+            Console.WriteLine("Converted: " + outputUrl);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("File conversion failed: " + e.Message);
+        }
     }
 }
